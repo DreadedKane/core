1e51f829a4ba9af83c9de340e20867977c108551
Eugene Bridger <eugene.bridger@gmail.com>
2018-08-30 10:20:22 +1200

    Added README. Added project.

---
+++ Assets/HomewreckersStudio/Core/Scripts/Event.cs
@@ -0,0 +1,45 @@
+﻿/**
+ * Copyright (c) Eugene Bridger. All rights reserved.
+ * Licensed under the MIT License. See LICENSE file in the project root for full license information.
+ */
+
+using System;
+
+namespace HomewreckersStudio
+{
+    public static class Event
+    {
+        /**
+         * Invokes an action if it's valid.
+         */
+        public static void Invoke(Action action)
+        {
+            if (action != null)
+            {
+                action();
+            }
+        }
+
+        /**
+         * Invokes an action with one parameter if it's valid.
+         */
+        public static void Invoke<T>(Action<T> action, T parameter)
+        {
+            if (action != null)
+            {
+                action(parameter);
+            }
+        }
+
+        /**
+         * Invokes an action with two parameters if it's valid.
+         */
+        public static void Invoke<T1, T2>(Action<T1, T2> action, T1 parameter1, T2 parameter2)
+        {
+            if (action != null)
+            {
+                action(parameter1, parameter2);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Core
+
+Unity package for global utilities.
+
+## Features
+* Helper class for invoking events.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit)
